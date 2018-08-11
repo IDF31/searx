78ab0332ac5a8402078ae5232a3ece369b1add2f
IDF31 <iorgadragos04@gmail.com>
2018-08-11 14:37:47 +0300

    Licensed plugins because plugins like LibreJS will block these plugins because they do not use a license

--- searx/static/plugins/js/infinite_scroll.js
+++ searx/static/plugins/js/infinite_scroll.js
@@ -1,3 +1,4 @@
+//@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3-0
 $(document).ready(function() {
     var win = $(window);
     win.scroll(function() {
@@ -16,3 +17,4 @@ $(document).ready(function() {
         }
     });
 });
+//@license-end
--- searx/static/plugins/js/vim_hotkeys.js
+++ searx/static/plugins/js/vim_hotkeys.js
@@ -1,3 +1,4 @@
+//@license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3-0
 $(document).ready(function() {
     highlightResult('top')();
 
@@ -335,3 +336,4 @@ $(document).ready(function() {
         $('body').append(html);
     }
 });
+//@license-end
